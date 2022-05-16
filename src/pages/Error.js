9ee9cdef50c4be7737089d5aa27d953ec218ecdb
naneu <nancysororo@gmail.com>
2022-05-16 10:05:10 +0300

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,17 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
-    return <h1>Error Page</h1>
+  return (
+    <section className="error-page section">
+    <div className="error-container">
+      <h1>Oops! page not found. </h1>
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
+    </div>
+    </section>
+  );
 }
 
 export default Error;
