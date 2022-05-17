749bd6c95dd2f9aef40b860b8f566864faee2a29
naneu <nancysororo@gmail.com>
2022-05-17 12:25:48 +0300

    add search by name functionality

--- src/components/Elephants.js
+++ src/components/Elephants.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Loading from "./Loading";
 import Elephant from "./Elephant";
 import { useGlobalContext } from "../Context";
 
 function Elephants() {
   const { elephants, loading } = useGlobalContext();
+  const [searchTerm, setSearchTerm] = useState("");
   if (loading) {
     return <Loading />;
   }
   return (
     <section className="section">
-      <h2 className="section-title">Elephants</h2>
+      <div className="header-section">
+        <h2 className="section-title">Elephants</h2>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
       <div className="elephants-center">
-        {elephants.map((item) => {
-          return <Elephant key={item.index} {...item} />;
-        })}
+        {elephants
+          .filter((item) => {
+            if (searchTerm === "") {
+              return item;
+            } else if (
+              item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+            ) {
+              return item;
+            }
+          })
+          .map((item) => {
+            return <Elephant key={item.index} {...item} />;
+          })}
       </div>
     </section>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { useGlobalContext } from "../Context";
-import Loading from "../components/Loading";
 import Elephants from "../components/Elephants";
 
 function Home() {
-  
   return (
     <div>
       <Elephants />
